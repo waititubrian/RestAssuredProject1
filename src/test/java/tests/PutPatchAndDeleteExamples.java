6d6943bd7f636ac8dbbe325580e6522afe907eac
waititubrian <waititubrian9@gmail.com>
2024-08-20 21:05:46 +0300

    Put, Patch and Delete methods added

---
+++ src/test/java/tests/PutPatchAndDeleteExamples.java
@@ -0,0 +1,69 @@
+package tests;
+
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
+
+public class PutPatchAndDeleteExamples {
+
+    @Test
+    public void testPut(){
+
+        JSONObject request = new JSONObject();
+
+        request.put("name", "Brian");
+        request.put("job", "Testing");
+
+        System.out.println(request.toJSONString());
+        baseURI = "https://reqres.in/api/users/2";
+
+        given().
+                header("Content-Type", "application/json").
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                body(request.toJSONString()).
+        when().
+                put(baseURI).
+        then().
+                statusCode(200).log().all();
+
+    }
+
+    @Test
+    public void testPatch(){
+
+        JSONObject request = new JSONObject();
+
+        request.put("name", "Brian");
+        request.put("job", "Testing");
+
+        System.out.println(request.toJSONString());
+        baseURI = "https://reqres.in/api/users/2";
+
+        given().
+                header("Content-Type", "application/json").
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                body(request.toJSONString()).
+        when().
+                patch(baseURI).
+        then().
+                statusCode(200).log().all();
+
+    }
+
+    @Test
+    public void testDelete(){
+
+        baseURI = "https://reqres.in/api/users/2";
+
+        when().
+                delete(baseURI).
+        then().
+                statusCode(204).log().all();
+
+    }
+}
